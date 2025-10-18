5b20fb544bbf3f700391d73a3eb015b45e5c8c1b
Vincent Sit <vincentsit00@ymail.com>
2025-10-19 12:52:38 +1300

    Add util methods to check types

--- src/util/checks.ts
+++ src/util/checks.ts
@@ -13,3 +13,10 @@ export function arraysEqual(a : any[], b : any[]) {
     }
     return true;
 }
+
+export const isNumber = (a): a is number => typeof a === 'number';
+export const isObject = (a): a is object => a instanceof Object;
+export const isBoolean = (arg): arg is boolean => arg === !!arg;
+export const isFunction = (val): val is Function => typeof val === 'function';
+export const isUndefined = (val): val is undefined => val === undefined;
+export const isNil = (val): val is null | undefined => val == null;
--- src/util/index.ts
+++ src/util/index.ts
@@ -1,3 +1,4 @@
 export * from './webgl';
 export * from './shaders';
 export * from './camera';
+export * from './checks';

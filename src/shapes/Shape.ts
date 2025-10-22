c830bbf0a1fa08d83763e6970b98c04a16e29366
Vincent Sit <vincentsit00@ymail.com>
2025-10-22 18:58:53 +1300

    Remove unused property

--- src/shapes/Shape.ts
+++ src/shapes/Shape.ts
@@ -16,8 +16,6 @@ export abstract class Shape extends WebGLRenderable {
     private _x: number;
     private _y: number;
     private _renderOrder: number = 0;
-    private _selected: boolean = false;
-
     abstract getVertexCount(): number;
 
     constructor(x: number, y: number) {
@@ -37,12 +35,6 @@ export abstract class Shape extends WebGLRenderable {
 
     get renderOrder() { return this._renderOrder; }
     set renderOrder(v: number) { if (this._renderOrder !== v) { this._renderOrder = v; this.renderDirtyFlag = true; } }
-
-    get selected() { return this._selected }
-    set selected(v: boolean) { 
-        this._selected = v;
-        this.renderDirtyFlag = true;
-    }
 
     get seq() { return this._seq; }
 
